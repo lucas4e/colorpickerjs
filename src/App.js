f0d89e0ffbdcf7df89151e7ecb0e3f9416640c40
Lucas <lucas.ersson@student.kyh.se>
2021-09-01 16:52:26 +0200

    added js

--- src/App.js
+++ src/App.js
@@ -5,19 +5,39 @@ import './index.css';
 
 function App() {
   const [hue, setHue] = useState(0);
-  const [saturation, setSaturation] = useState(0);
+  const [saturation, setSaturation] = useState(50);
   const [lightness, setLightness] = useState(0);
 
   const mousePos = useMousePosition();
   const scrollVal = useScrollValue();
 
+  useEffect(() => {
+    const mousePosDivByWindow = mousePos.y / window.innerHeight;
+    const newSaturation = mousePosDivByWindow * 100;
+    setSaturation(newSaturation);
+  }, [mousePos.y]);
+
+  useEffect(() => {
+    const mousePosDivByWindow = mousePos.x / 360;
+    const newHue = mousePosDivByWindow * 100;
+    setHue(newHue);
+  }, [mousePos.x]);
+
   const getHSL = (h, s, l) => {
-    h = 220;
-    s = 70;
+    h = hue;
+    s = saturation;
     l = 50;
 
     return `hsl(${h}, ${s}%, ${l}%)`;
   };
+
+  if (saturation > 100) {
+    return setSaturation(100);
+  }
+
+  if (saturation < 0) {
+    return setSaturation(0);
+  }
 
   return (
     <div className='container'>
@@ -25,7 +45,9 @@ function App() {
         className='background'
         style={{ backgroundColor: getHSL(hue, saturation, lightness) }}
       >
-        {console.log(mousePos.x + ' ' + mousePos.y)}
+        <div className='colorProps'>
+          <p>{'Saturation: ' + Math.round(saturation)}</p>
+        </div>
       </div>
     </div>
   );
