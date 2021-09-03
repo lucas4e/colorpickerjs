80baf3824881d1d0fd9927170d1b15f0a4b186d4
Lucas <lucas.ersson@student.kyh.se>
2021-09-03 23:21:06 +0200

    added conditional rendering and copy to clipboard functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect, useMemo, useRef } from 'react';
 import { useMousePosition } from './components/useMousePosition';
 import { useScrollValue } from './components/useScrollValue';
 import { setConstraints } from './components/setConstraints';
@@ -7,6 +7,7 @@ import { useCalculateHEX } from './components/useCalculateHEX';
 import './index.css';
 
 function App() {
+  const [hasMoved, setHasMoved] = useState(false);
   const [hue, setHue] = useState(0);
   const [saturation, setSaturation] = useState(0);
   const [lightness, setLightness] = useState(0);
@@ -57,12 +58,23 @@ function App() {
     return `hsl(${h}, ${s}%, ${l}%)`;
   };
 
+  const handleClick = (e) => {
+    navigator.clipboard.writeText(e).then(
+      () => {
+        console.log('Copied ' + e + ' to clipboard');
+      },
+      () => {
+        console.log('Copy to clipboard failed');
+      }
+    );
+  };
+
   const displayHSL = () => {
     return `hsl(${Math.round(hue)}, ${Math.round(saturation)}%, ${lightness}%)`;
   };
 
-  const rgb = useCalculateRGB(element);
-  const hex = useCalculateHEX(element);
+  let rgb = useCalculateRGB(element);
+  let hex = useCalculateHEX(element);
 
   return (
     <div className='container'>
@@ -72,9 +84,21 @@ function App() {
         style={{ backgroundColor: getHSL(hue, saturation, lightness) }}
       >
         <div className='colorProps disable-select'>
-          <p>{displayHSL()}</p>
-          <p>{rgb}</p>
-          <p>{hex}</p>
+          {!hasMoved ? (
+            <div>Mouse cursor to change color</div>
+          ) : (
+            <div>
+              <p onClick={(e) => handleClick(e.target.innerHTML)}>
+                {displayHSL()}
+              </p>
+              <p onClick={(e) => handleClick(e.target.innerHTML)}>
+                {rgb === 'rgb(0, 0, 0)' ? (rgb = 'rgb(64, 191, 191)') : rgb}
+              </p>
+              <p onClick={(e) => handleClick(e.target.innerHTML)}>
+                {hex === '#000000' ? (hex = '#40bfbf') : hex}
+              </p>
+            </div>
+          )}
         </div>
       </div>
     </div>
