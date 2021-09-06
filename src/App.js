4dfdb74e7f07b5ccaa6061f806ba9c9bea495b47
Lucas <lucas.ersson@student.kyh.se>
2021-09-06 16:17:37 +0200

    added styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect, useMemo, useRef } from 'react';
 import { useMousePosition } from './components/useMousePosition';
 import { useScrollValue } from './components/useScrollValue';
 import { setConstraints } from './components/setConstraints';
@@ -7,10 +7,11 @@ import { useCalculateHEX } from './components/useCalculateHEX';
 import './index.css';
 
 //Feather Icons
-import { Clipboard, HelpCircle, Copy } from 'react-feather';
+import { Check, HelpCircle, Copy } from 'react-feather';
 
 function App() {
   const [hasMoved, setHasMoved] = useState(false);
+  const [hoverOnOutput, setHoverOnOutput] = useState(false);
   const [hue, setHue] = useState(0);
   const [saturation, setSaturation] = useState(0);
   const [lightness, setLightness] = useState(0);
@@ -19,16 +20,22 @@ function App() {
   const mousePos = useMousePosition();
   const scrollVal = useScrollValue();
 
-  document.addEventListener('mousedown', () => {
-    setHasMoved(true);
-  });
-
-  document.addEventListener('wheel', () => {
-    setHasMoved(true);
-  });
+  const { HSLRef } = useRef(null);
+  const { HEXRef } = useRef(null);
+  const { RGBRef } = useRef(null);
 
   const element = document.getElementById('background');
   const copy = document.getElementById('copyPopup');
+  const colorBox = document.getElementById('colorProps');
+
+  const listenForMove = () => {
+    setHasMoved(true);
+    document.removeEventListener('mousedown', listenForMove);
+    document.removeEventListener('wheel', listenForMove);
+  };
+
+  document.addEventListener('mousedown', listenForMove, { once: true });
+  document.addEventListener('wheel', listenForMove, { once: true });
 
   const constraints = useMemo(() => {
     const props = {
@@ -47,20 +54,32 @@ function App() {
   setConstraints(constraints);
 
   useEffect(() => {
-    const mousePosDivByWindow = mousePos.y / window.innerHeight;
-    const newSaturation = mousePosDivByWindow * 100;
-    setSaturation(newSaturation);
+    if (!hoverOnOutput) {
+      const mousePosDivByWindow = mousePos.y / window.innerHeight;
+      const newSaturation = mousePosDivByWindow * 100;
+      setSaturation(newSaturation);
+    }
   }, [mousePos.y]);
 
   useEffect(() => {
-    const mousePosDivByWindow = mousePos.x / window.innerWidth;
-    const newHue = mousePosDivByWindow * maxHue;
-    setHue(newHue);
+    if (!hoverOnOutput) {
+      const mousePosDivByWindow = mousePos.x / window.innerWidth;
+      const newHue = mousePosDivByWindow * maxHue;
+      setHue(newHue);
+    }
   }, [mousePos.x]);
 
   useEffect(() => {
     setLightness(scrollVal);
   }, [scrollVal]);
+
+  useEffect(() => {
+    if (colorBox) {
+      colorBox.addEventListener('mousedown', (e) => {
+        e ? setHoverOnOutput(true) : setHoverOnOutput(false);
+      });
+    }
+  }, [mousePos]);
 
   const getHSL = (h, s, l) => {
     h = hue;
@@ -100,22 +119,47 @@ function App() {
         id='background'
         style={{ backgroundColor: getHSL(hue, saturation, lightness) }}
       >
-        <div className='logo absolute'>
-          <p>ColorPickerJS</p>
-        </div>
         <div className='about'>
           <HelpCircle />
         </div>
-        <div className='colorProps absolute disable-select cursor'>
+        <div
+          id='colorProps'
+          className='colorProps absolute disable-select cursor'
+        >
           {!hasMoved ? (
             <div>Click and drag cursor to change the background color</div>
           ) : (
             <div>
-              <p onClick={(e) => handleClick(e.target.innerHTML)}>
-                {displayHSL()}
-              </p>
-              <p onClick={(e) => handleClick(e.target.innerHTML)}>{rgb}</p>
-              <p onClick={(e) => handleClick(e.target.innerHTML)}>{hex}</p>
+              <span>
+                <span>
+                  <h2>HSL</h2>
+                  <p ref={HSLRef} value='HSL'>
+                    {displayHSL()}
+                  </p>
+                </span>
+                <Copy onClick={() => handleClick(HSLRef)} />
+              </span>
+              <div className='line'></div>
+              <span>
+                <span>
+                  <h2>RGB</h2>
+                  <p ref={RGBRef} value='RGB'>
+                    {rgb}
+                  </p>
+                </span>
+                <Copy onClick={() => handleClick()} />
+              </span>
+              <div className='line'></div>
+              <span>
+                <span>
+                  <h2>HEX</h2>
+                  <p ref={HEXRef} value='HEX'>
+                    {hex}
+                  </p>
+                </span>
+                <Copy onClick={() => handleClick()} />
+              </span>
+              <div className='line'></div>
             </div>
           )}
         </div>
@@ -125,7 +169,7 @@ function App() {
         >
           <div>
             <p>Copied</p>
-            <Clipboard />
+            <Check />
           </div>
         </div>
       </div>
